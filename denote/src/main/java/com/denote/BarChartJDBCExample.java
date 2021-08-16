82e716d2788fc3529999cb3e811dbffd66a799b0
Tanvir-1999 <skatta3@yahoo.com>
2021-08-16 09:09:44 +0530

    Verifying Commit

--- denote/src/main/java/com/denote/BarChartJDBCExample.java
+++ denote/src/main/java/com/denote/BarChartJDBCExample.java
@@ -15,6 +15,10 @@ public class BarChartJDBCExample {
         
         public static void main(String[] args) throws Exception{
                 
+        	
+        	
+        	
+        	
                 Class.forName ("com.mysql.jdbc.Driver"); 
                 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jayesh", "root", "admin");
                 DefaultCategoryDataset my_bar_chart_dataset = new DefaultCategoryDataset();
